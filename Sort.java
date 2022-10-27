ba6a389f088ac2f75e86bda9fcbd0cfccd3237c7
Ilya Andreev <mrbrikster@yandex.ru>
2022-10-27 20:43:23 +0300

    Rewrite to Shellsort

--- Sort.java
+++ Sort.java
@@ -1,15 +1,29 @@
 public class Sort {
 
     public static int[] sort(int[] mas) {
-        for (int i = 0; i < mas.length; i++) {
-            for (int j = 1; j < mas.length; j++) {
-                if (mas[i] > mas[j]) {
-                    int tmp = mas[j];
-                    mas[j] = mas[i];
-                    mas[i] = tmp;
-                }
-            }
+        // Shellsort impl
+        int h = 1;
+
+        while (h <= mas.length / 3) {
+            h = h * 3 + 1;
         }
+
+        while (h > 0) {
+            for (int outer = h; outer < mas.length; outer++) {
+                int tmp = mas[outer];
+                int inner = outer;
+
+                while (inner > h - 1 && mas[inner - h] > tmp) {
+                    mas[inner] = mas[inner - h];
+                    inner -= h;
+                }
+
+                mas[inner] = tmp;
+            }
+
+            h = (h - 1) / 3;
+        }
+        
         return mas;
     }
 }

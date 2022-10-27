02ac8370b3e49cf0487c819465bc20b25f70c0ea
vikitory <v.cheloshkina@yandex.ru>
2022-10-27 21:46:36 +0300

    Rewrite to insertion sort

--- Sort.java
+++ Sort.java
@@ -1,15 +1,14 @@
 public class Sort {
 
     public static int[] sort(int[] mas) {
-        for (int i = 0; i < mas.length; i++) {
-            for (int j = 1; j < mas.length; j++) {
-                if (mas[i] > mas[j]) {
-                    int tmp = mas[j];
-                    mas[j] = mas[i];
-                    mas[i] = tmp;
-                }
-            }
+    int j;
+    for (int i = 1; i < sortArr.length; i++) {
+        int swap = sortArr[i];
+        for (j = i; j > 0 && swap < sortArr[j - 1]; j--) {
+            sortArr[j] = sortArr[j - 1];
         }
+        sortArr[j] = swap;
+    }
         return mas;
     }
 }

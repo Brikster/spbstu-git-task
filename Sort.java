bd5d04776b02d84a35fe976dbc6fb1fde4e83bd9
Lotsn <dimit202@yandex.ru>
2022-10-28 00:11:35 +0300

    QuickSort

--- Sort.java
+++ Sort.java
@@ -1,15 +1,37 @@
 public class Sort {
 
     public static int[] sort(int[] mas) {
-        for (int i = 0; i < mas.length; i++) {
-            for (int j = 1; j < mas.length; j++) {
-                if (mas[i] > mas[j]) {
-                    int tmp = mas[j];
-                    mas[j] = mas[i];
-                    mas[i] = tmp;
-                }
+        quicksort(mas, 0, mas.length - 1);
+        return mas;
+    }
+
+    public static void quicksort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quicksort(arr, begin, partitionIndex-1);
+            quicksort(arr, partitionIndex+1, end);
+        }
+    }
+
+    private static int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin-1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
             }
         }
-        return mas;
+
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i+1;
     }
 }
